ea31d35bb5208eb5954e6c07a9a7a25037924b43
guino henostroza <guinohenostroza@gmail.com>
2015-10-14 18:32:27 -0700

    Nuevo3

---
+++ SistemaReservas.AccesoDatos.Interfaces/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaReservas.AccesoDatos.Interfaces
+{
+    public class Class1
+    {
+    }
+}
---
+++ SistemaReservas.AccesoDatos.Interfaces/IClienteDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaReservas.Entidades;
+
+namespace SistemaReservas.AccesoDatos.Interfaces
+{
+    interface IClienteDAO
+    {
+        public IEnumerable<Cliente> LeerTodos();
+        public Cliente LeerCliente(Cliente cliente);
+        public int Insertar(Cliente cliente);
+        public bool Actualizar(Cliente cliente);
+        public bool Eliminar(Cliente cliente);
+    }
+}
