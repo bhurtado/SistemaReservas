58434e7ba37bfc791ae87bfeaa065154b94d9439
guino henostroza <guinohenostroza@gmail.com>
2015-10-14 18:11:55 -0700

    validaciones 1

---
+++ SistemaReservas.Validacion/ClienteMetadata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaReservas.Validacion
+{
+    public class ClienteMetadata
+    {
+        [Display(Name = "Nombre:")]
+        [Required(ErrorMessage = "Se requiere un nombre")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 50 caracteres")]
+        public string Nombre { get; set; }
+    }
+}
