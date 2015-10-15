8b97591c6fa80f487d56773909e4ffc6a1b5839e
guino henostroza <guinohenostroza@gmail.com>
2015-10-14 18:40:48 -0700

    ClienteDAO

--- SistemaReservas.AccesoDatos/ClienteDAO.cs
+++ SistemaReservas.AccesoDatos/ClienteDAO.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SistemaReservas.Entidades;
+using SistemaReservas.AccesoDatos.Interfaces;
 
 namespace SistemaReservas.AccesoDatos
 {
-    class ClienteDAO
+    public class ClienteDAO : IClienteDAO
     {
+
+        public IEnumerable<Cliente> LeerTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente LeerCliente(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insertar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Actualizar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Eliminar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
