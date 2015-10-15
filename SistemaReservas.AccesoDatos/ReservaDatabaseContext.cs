a66a9fc0b71fc2bb0d9958f60d91b3bd4557363c
guino henostroza <guinohenostroza@gmail.com>
2015-10-14 18:48:59 -0700

    aa

---
+++ SistemaReservas.AccesoDatos/ReservaDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using SistemaReservas.Entidades;
+
+namespace SistemaReservas.AccesoDatos
+{
+    public class ReservaDatabaseContext : DbContext
+    {
+        public ReservaDatabaseContext() :
+            base("name=ReservaDatabase")
+        { }
+        public DbSet<Cliente> Clientes { get; set; }
+        //
+    }
+}
