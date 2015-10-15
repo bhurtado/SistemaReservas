ec624421c1a0d108e467eb84dc675e90d76e43f9
guino henostroza <guinohenostroza@gmail.com>
2015-10-14 19:09:21 -0700

    ss

---
+++ SistemaReservas.LogicaNegocio.Interfaces/IClienteLN.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaReservas.Entidades;
+
+namespace SistemaReservas.LogicaNegocio.Interfaces
+{
+    public interface IClienteLN
+    {
+        IEnumerable<Cliente> LeerTodos();
+        Cliente LeerCliente(Cliente cliente);
+        int Insertar(Cliente cliente);
+        bool Actualizar(Cliente cliente);
+        bool Eliminar(Cliente cliente);
+    }
+}
